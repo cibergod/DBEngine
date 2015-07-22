338e1820b4a93914bf47f4d1d51240bf88b4ff8e
cibergod <cibergod1@gmail.com>
2015-07-22 11:10:05 +0300

    Сохранение результатов поиска о Kliko в XML файл в виде таблицы
    Добавлена возможность поиска Kliko и сохранение данных о найденных
    отчетностях в XML файл

--- DBReaderLoader/DBReaderLoader/Program.cs
+++ DBReaderLoader/DBReaderLoader/Program.cs
@@ -9,11 +9,10 @@ namespace DBReaderLoader
         static void Main(string[] args)
         {
             //обьявим класс
-            DBXML MyCreateTable = new DBXML();
+            //DBXML MyCreateTable = new DBXML();
             //запустим создание таблицы и сохранение ее в файл
             //MyCreateTable.CreateFerstTable();
             //загрузка таблицы MyTestTable из файла
-            
         }
     }
 }
