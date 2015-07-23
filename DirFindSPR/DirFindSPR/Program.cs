d2be6d66561bfc6fe52b9318ccb8c7ed638bab4e
cibergod <cibergod1@gmail.com>
2015-07-23 17:23:17 +0300

    Новый проект

---
+++ DirFindSPR/DirFindSPR/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DirFindSPR
+{
+
+
+
+    class Program
+    {
+        static string PATCH = "";
+        static DirectoryInfo MyDir;
+        static void DirList()
+        {
+            string[] ListDir = Directory.GetDirectories(PATCH);
+
+            string Name = "";
+            foreach (string N in ListDir)
+            {
+             //   Console.WriteLine(N);
+                MyDir = new DirectoryInfo(N);
+                try
+                {
+                    Convert.ToInt32(MyDir.Name);
+                    Console.WriteLine(MyDir.Name);
+                }
+                catch 
+                { 
+                    //не подходит 
+                }
+
+
+
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PATCH = @"o:\162T_01\DATA\KZN\";
+            DirList();
+
+            Console.ReadKey();
+        }
+    }
+}
